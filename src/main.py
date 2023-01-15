5817658e419a9689c38192872a10305a06150003
Lawrence Lim <ll4715@nyu.edu>
2023-01-15 22:33:07 +0800

    Updated stuff?

--- src/main.py
+++ src/main.py
@@ -4,9 +4,19 @@ import json
 import requests
 import os
 import sqlite3
+import xlsxwriter
+import openpyxl
 
 load_dotenv()
 API_KEY = os.getenv("API_KEY")
+
+
+statement_types = [
+    "balance-sheet-statement",
+    "cash-flow-statement",
+    "income-statement",
+    "quote",
+]
 
 
 def get_company_tickers(
@@ -23,7 +33,7 @@ def get_company_tickers(
     return companies
 
 
-def _load_data(
+def _load_from_json(
     company: str, statement_type: str, data_dirname: str = "data"
 ) -> list[dict[str, int | str]]:
     """
@@ -35,7 +45,7 @@ def _load_data(
     return data[company]
 
 
-def _fetch_data(company: str, statement_type: str) -> list[dict[str, int | str]]:
+def _fetch_from_api(company: str, statement_type: str) -> list[dict[str, int | str]]:
     """
     Fetches data from the API and returns a dictionary
     """
@@ -74,27 +84,108 @@ def _save_to_db(
 
 def get_financials(
     companies: list[str],
-    statement_types: list[str] = [
-        "balance-sheet-statement",
-        "cash-flow-statement",
-        "income-statement",
-        "quote",
-    ],
 ) -> None:
     """
     Fetches data with API and writes to local sqlite3 database
     """
     for statement_type in statement_types:
-        for company in tqdm(companies, desc=f"Updating {statement_type} file"):
-            # financial_statement = _fetch_data(company, statement_type)
-            financial_statement_data = _load_data(company, statement_type)
+        for company in tqdm(companies, desc=f"Writing {statement_type} data"):
+            financial_statement_data = _fetch_from_api(company, statement_type)
+            # financial_statement_data = _load_from_json(company, statement_type)
             for year_index in range(len(financial_statement_data)):
                 _save_to_db(financial_statement_data[year_index], statement_type)
 
 
-def main():
+def _comparable_analysis_helper(
+    company: str,
+    db_path: str = "/Users/lawrencelim/Projects/python-projects/pyfinny/financial_data.db",
+) -> tuple:
+    """
+    Writes data from sqlite3 database to xlsx file
+    """
+
+    # Connect to database
+    conn = sqlite3.connect(db_path)
+    cursor = conn.cursor()
+
+    quote_dict = {
+        "price": 0.0,
+        "marketCap": 0,
+    }
+    income_statement_dict = {
+        "netIncome": 0,
+        "revenue": 0,
+        "ebitda": 0,
+        "depreciationAndAmortization": 0,
+    }
+    balance_sheet_dict = {
+        "cashAndCashEquivalents": 0,
+        "totalDebt": 0,
+    }
+    cash_flow_statement_dict = {}
+
+    # Fetch corresponding data from database and add it to the dictionary
+    for value_type in quote_dict:
+        cursor.execute(f"SELECT {value_type} FROM quote WHERE symbol = '{company}'")
+        quote_dict[value_type] = cursor.fetchone()[0]
+    for value_type in income_statement_dict:
+        cursor.execute(
+            f"SELECT {value_type} FROM income_statement WHERE symbol = '{company}'"
+        )
+        income_statement_dict[value_type] = cursor.fetchone()[0]
+    for value_type in balance_sheet_dict:
+        cursor.execute(
+            f"SELECT {value_type} FROM balance_sheet WHERE symbol = '{company}'"
+        )
+        balance_sheet_dict[value_type] = cursor.fetchone()[0]
+    for value_type in cash_flow_statement_dict:
+        cursor.execute(
+            f"SELECT {value_type} FROM cash_flow_statement WHERE symbol = {company}"
+        )
+        cash_flow_statement_dict[value_type] = cursor.fetchone()[0]
+
+    return (
+        quote_dict,
+        income_statement_dict,
+        balance_sheet_dict,
+        cash_flow_statement_dict,
+    )
+
+
+def comparables_analysis(companies: list[str]) -> None:
+    """
+    Writes data from sqlite3 database to xlsx file
+    """
+    for company in companies:
+        (
+            quote_dict,
+            income_statement_dict,
+            balance_sheet_dict,
+            cash_flow_statement_dict,
+        ) = _comparable_analysis_helper(company)
+
+        price = quote_dict["price"]
+        marketCap = quote_dict["marketCap"]
+        ev = (
+            marketCap
+            + balance_sheet_dict["totalDebt"]
+            - balance_sheet_dict["cashAndCashEquivalents"]
+        )
+        ebitda = income_statement_dict["ebitda"]
+        ebit = ebitda - income_statement_dict["depreciationAndAmortization"]
+        revenue = income_statement_dict["revenue"]
+        earnings = income_statement_dict["netIncome"]
+
+
+def load_companies():
     companies = get_company_tickers()
     get_financials(companies)
+
+
+def main():
+    # companies = get_company_tickers()
+    # comparables_analysis(companies)
+    get_financials(['NVDA'])
 
 
 if __name__ == "__main__":
