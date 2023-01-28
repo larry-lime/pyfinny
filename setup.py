477df089a97befc64c77bb961a3c317add8f106e
Lawrence Lim <ll4715@nyu.edu>
2023-01-28 16:28:04 +0800

    Updated setup

---
+++ setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+with open("requirements.txt", "r", encoding="utf-8") as fh:
+    requirements = fh.read()
+
+setup(
+    name="pyfinny",
+    version="1.0.0",
+    author="Lawrence Lim",
+    author_email="ll4715@nyu.edu",
+    license="GNU GPLv3",
+    description="Financial Statement Analyzer CLI",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/larry-lime/pyfinny",
+    py_modules=["pyfinny", "app"],
+    packages=find_packages(),
+    install_requires=requirements,
+    python_requires=">=3.7",
+    classifiers=[
+        "Programming Language :: Python :: 3.10",
+        "Operating System :: OS Independent",
+    ],
+    entry_points="""
+        [console_scripts]
+        pyfin=pyfinny:cli
+    """,
+)
